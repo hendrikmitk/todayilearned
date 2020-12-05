da42c86d75f9e53936b501aafbde41fb6d3e6466
Hendrik H <hharlichs@googlemail.com>
2020-12-05 13:03:56 +0100

    fix: presentation on iphone displays

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const spinnerWrapper = document.querySelector("#spinner-wrapper");
 /////////////
 
 const insertTilHtml = (elem, til) => {
-	elem.innerHTML = `<div class="h-full flex flex-col justify-center items-start px-14"><header class="font-bold text-xl md:text-2xl lg:text-3xl pb-4">r/todayilearned</header><p class="sm:text-lg pt-4">${til}</p></div>`;
+	elem.innerHTML = `<div class="h-full flex flex-col justify-center items-start px-8 md:px-14"><header class="font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl pb-2 md:pb-4">r/todayilearned</header><p class="text-sm sm:text-base md:text-lg pt-2 md:pt-4">${til}</p></div>`;
 };
 
 /////////////
